c19814d1ea9ce944a952cc0b0d7ed3e61c7efb01
drkrieger <eplyushko@gmail.com>
2017-10-31 12:39:47 -0700

    project start, added mvc into startup and created a cottroller folder with a tasks controller

---
+++ Lab36Erik/Controllers/TasksController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab36Erik.Controllers
+{
+    public class TasksController
+    {
+    }
+}
