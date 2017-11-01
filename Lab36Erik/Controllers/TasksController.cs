c4dfac017e5dd32dc9f8b6cbaf091a14ce32d76e
drkrieger <eplyushko@gmail.com>
2017-10-31 22:06:54 -0700

    model added along wiht db context, tests and migration

--- Lab36Erik/Controllers/TasksController.cs
+++ Lab36Erik/Controllers/TasksController.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lab36Erik.Controllers
 {
-    public class TasksController
+    [Route("api/[Controller]")]
+    public class TasksController : ControllerBase
     {
+        //Get
+        [HttpGet("{id:int?}")] 
+        public string Get(string id)
+        {
+            return "Got this";
+        }
+
+        //Post 
+
+        //Put 
+
+        //Delete 
     }
 }
